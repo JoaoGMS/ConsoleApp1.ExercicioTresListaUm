2ada4a9706b0577312fc6af84483cde22aab1d8b
joaog <joaog@DESKTOP-ECAA0L7>
2022-02-18 16:50:10 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1.ExercicioTresListaUm/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApp1.ExercicioTresListaUm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("Dias de vida");
+            while (true)
+            {
+               
+                string nome;
+                int idade, diasDeVida;
+
+                Console.Write("Por favor, informe seu nome: ");
+                nome = Console.ReadLine();
+
+                Console.Write("\nAgora informe sua idade: ");
+                idade = Convert.ToInt32(Console.ReadLine());
+
+                diasDeVida = idade * 365;
+
+                Console.WriteLine(nome + ", você já viveu " + diasDeVida + " dias");
+                Console.WriteLine("=================================");
+
+                Console.WriteLine("Aperte ENTER para fazer novamente");
+                Console.ReadLine();
+
+            }while(true);   
+        }
+    }
+}
